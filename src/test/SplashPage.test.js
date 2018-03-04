ab910bea12f6eb7958c5aacfd516110fb5644344
Kevin Lim <knlim95@gmail.com>
2018-03-04 12:37:41 -0800

    Prep for Jest Testing

---
+++ src/test/SplashPage.test.js
@@ -0,0 +1,9 @@
+// import expect from 'expect';
+import React from 'react';
+// import renderer from 'react-test-renderer';
+
+import SplashPage from '../components/home/SplashPage';
+
+test('test of a test', () => {
+  true;
+});
