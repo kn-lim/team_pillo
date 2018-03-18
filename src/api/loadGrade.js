77c88bd55e2316936d57b955e755f53022eb50ae
Kevin Lim <knlim95@gmail.com>
2018-03-18 01:03:14 -0700

    pre-req for ajax

--- src/api/loadGrade.js
+++ src/api/loadGrade.js
@@ -20,6 +20,32 @@ class MemberApi {
       }, delay);
     });
   }
+
+  static saveMember(members) {
+    members = Object.assign({}, members); // to avoid manipulating object passed in.
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        //Just simulating creation here.
+        //The server would generate ids for new authors in a real app.
+        //Cloning so copy returned is passed by value rather than by reference.
+        members.push(members);
+
+        resolve(members);
+      }, delay);
+    });
+  }
+
+  static deleteMembers(members) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const indexOfMembersToDelete = members.findIndex(members => {
+          return members == members;
+        });
+        members.splice(indexOfMembersToDelete, 1);
+        resolve();
+      }, delay);
+    });
+  }
 }
 
-export default MsgApi;
+export default MembersApi;
